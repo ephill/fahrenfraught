b87ca38351ac680854b39080e7cf753deb841f76
Evan Phillips <ephill18@gmail.com>
2024-08-22 16:23:38 -0700

    Add icon to header.

--- src/components/pageHeader.tsx
+++ src/components/pageHeader.tsx
@@ -1,9 +1,11 @@
 import { ModeToggle } from "@/components/modeToggle";
+import { Thermometer } from "lucide-react";
 
 export const PageHeader = () => {
   return (
     <header className="sticky top-0 z-50">
       <div className="container flex h-14 max-w-screen-2xl items-center">
+        <Thermometer className="stroke-red-400" />
         <div className="mr-4">Fahrenfraught</div>
         <div className="flex flex-1 items-end justify-end">
           <ModeToggle />
