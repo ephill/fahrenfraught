cf6d55ec212f6b20b2e7941669ea955dbcfcaf6b
Evan Phillips <ephill18@gmail.com>
2024-08-22 22:00:31 -0700

    Add degree symbol and units to temperature.

--- src/components/forecastPeriod.tsx
+++ src/components/forecastPeriod.tsx
@@ -1,14 +1,24 @@
 import { convertFahrenheitToCelsius } from "@/lib/convertTemp";
 import type { components } from "@/lib/weather-gov-schema";
+import { useMemo } from "react";
 
 type Period = components["schemas"]["GridpointForecastPeriod"];
 
 export const ForecastPeriod = (props: { period: Period }) => {
   const { period } = props;
+
+  // TODO (EvanP): Temperature as number is deprecated, switch to quantitative
+  // value
+  const temperature = period.temperature as number;
+
+  const temperatureDisplay = useMemo(() => {
+    const t = temperature as number;
+    return `${t}° F / ${convertFahrenheitToCelsius(t)}° C`;
+  }, [temperature]);
+
   return (
-    <div key={period.number}>
-      {period.name} = {period.temperature as number} /{" "}
-      {convertFahrenheitToCelsius(period.temperature as number)}
+    <div>
+      {period.name} = {temperatureDisplay}
     </div>
   );
 };
