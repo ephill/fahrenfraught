5706ae26916d0d7e7f6073e392345eda5cf650c9
Evan Phillips <ephill18@gmail.com>
2024-08-22 20:32:05 -0700

    Add types for the weather API.

--- src/components/weather.tsx
+++ src/components/weather.tsx
@@ -1,36 +1,47 @@
 import { convertFahrenheitToCelsius } from "@/lib/convertTemp";
 import { GeoLocation } from "@/lib/geoLocation";
-import { getWeatherData } from "@/lib/getWeatherData";
-import { useEffect, useState } from "react";
+import {
+  GetForecastResponse,
+  GetPointResponse,
+  getWeatherData,
+  RelativeLocation,
+} from "@/lib/getWeatherData";
+import { useEffect, useMemo, useState } from "react";
 
 export const Weather = (props: { location: GeoLocation }) => {
   const { location } = props;
 
-  const [periods, setPeriods] = useState<any>();
-  const [relativeLocation, setRelativeLocation] = useState<any>();
+  const [forecast, setForecast] = useState<GetForecastResponse>();
+  const [point, setPoint] = useState<GetPointResponse>();
   useEffect(() => {
     getWeatherData(location).then((res) => {
-      setPeriods(res.periods);
-      setRelativeLocation(res.relativeLocation);
+      setForecast(res.forecast);
+      setPoint(res.point);
     });
   }, [location]);
 
-  if (!relativeLocation && !periods) {
+  const cityState = useMemo(() => {
+    if (point) {
+      const relativeLocation = point.properties
+        .relativeLocation as RelativeLocation;
+
+      return `${relativeLocation.properties.city}, ${relativeLocation.properties.state}`;
+    }
+  }, [point]);
+
+  if (!point || !forecast) {
     return null;
   }
 
   return (
     <>
-      <div>
-        Showing location data for {relativeLocation.properties.city},{" "}
-        {relativeLocation.properties.state}
-      </div>
+      <div>Showing location data for {cityState}</div>
       <div className="flex flex-col">
-        {periods &&
-          periods.map((p: any) => (
+        {forecast.properties?.periods &&
+          forecast.properties.periods.map((p) => (
             <div key={p.number}>
-              {p.name} = {p.temperature} /{" "}
-              {convertFahrenheitToCelsius(p.temperature)}
+              {p.name} = {p.temperature as number} /{" "}
+              {convertFahrenheitToCelsius(p.temperature as number)}
             </div>
           ))}
       </div>
--- src/lib/getWeatherData.ts
+++ src/lib/getWeatherData.ts
@@ -1,17 +1,22 @@
 import { GeoLocation } from "@/lib/geoLocation";
+import type { components, paths } from "@/lib/weather-gov-schema";
+
+export type GetPointResponse =
+  paths["/points/{point}"]["get"]["responses"]["200"]["content"]["application/geo+json"];
+
+export type RelativeLocation = components["schemas"]["RelativeLocationGeoJson"];
+
+export type GetForecastResponse =
+  paths["/gridpoints/{wfo}/{x},{y}/forecast"]["get"]["responses"]["200"]["content"]["application/geo+json"];
 
 export async function getWeatherData(location: GeoLocation) {
-  const ret: any = {};
-  ret.periods = await fetch(
+  const point = (await fetch(
     `https://api.weather.gov/points/${location.latitude},${location.longitude}`,
-  )
-    .then((res) => res.json())
-    .then((json) => {
-      ret.relativeLocation = json.properties.relativeLocation;
-      return fetch(json.properties.forecast);
-    })
-    .then((res) => res.json())
-    .then((json) => json.properties.periods);
+  ).then((res) => res.json())) as GetPointResponse;
 
-  return ret;
+  const forecast = (await fetch(point.properties.forecast!).then((res) =>
+    res.json(),
+  )) as GetForecastResponse;
+
+  return { point, forecast };
 }
