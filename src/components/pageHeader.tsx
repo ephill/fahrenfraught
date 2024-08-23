55b12356430c0ca75c4c0f4c82ffba73f59fa135
Evan Phillips <ephill18@gmail.com>
2024-08-22 23:12:37 -0700

    Make icon lilac.

--- src/components/pageHeader.tsx
+++ src/components/pageHeader.tsx
@@ -5,8 +5,8 @@ export const PageHeader = () => {
   return (
     <header className="sticky top-0 z-50">
       <div className="container flex h-14 max-w-screen-2xl items-center">
-        <Thermometer className="stroke-red-400" />
-        <div className="mr-4">Fahrenfraught</div>
+        <Thermometer className="stroke-purple-400" />
+        <div className="mr-4 text-xl">Fahrenfraught</div>
         <div className="flex flex-1 items-end justify-end">
           <ModeToggle />
         </div>
